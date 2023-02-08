c48c1b933807334e2248b9723071c298a44fbde7
sebasrg89 <sebastian.rios.g@outlook.com>
2023-02-08 14:56:43 -0600

    crear fondo, añadir colision, añadir meteoritos, crear el puntaje

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame as pg
 from pygame.locals import *
 import random
+import time
+import sys
 
 pg.init()
 
@@ -8,6 +10,9 @@ FPS = 60
 framesPerSec = pg.time.Clock()
 
 NEGRO = (0,0,0)
+BLANCO = (255,255,255)
+VERDE = (0,128,94)
+ROJO = (255,0,0)
 
 pantalla = pg.display.set_mode((800,530))
 pantalla.fill(NEGRO)
@@ -20,12 +25,14 @@ class Meteoritos(pg.sprite.Sprite):
         super().__init__()
         self.image = pg.image.load("images/asteroide_mediano.png")
         self.surf = pg.Surface((40,40))
-        self.rect = self.surf.get_rect(center = (random.randint(40,460), 0))
+        self.rect = self.surf.get_rect(center = (random.randint(40,500), (random.randint(-100,0))))
 
-    def mover(self):
+    def mover(self, score):
         self.rect.move_ip(0,10)
         if (self.rect.bottom > 530):
-            self.rect.center = (random.randint(30,460), 0)
+            self.rect.center = (random.randint(30,460), (random.randint(-100,0)))
+            score += 1
+        return score
 
     def dibujar(self, surface):
         surface.blit(self.image, self.rect)
@@ -51,21 +58,84 @@ class Nave(pg.sprite.Sprite):
             if pressedKeys[K_RIGHT]:
                 self.rect.move_ip(5,0)
 
+class Fondo_Pantalla():
+    def __init__(self):
+        self.fondoPartida = pg.image.load("images/partida.png")
+        self.rectFPimage = self.fondoPartida.get_rect()
+
+        self.fpY1 = 0
+        self.fpX1 = 0
+
+        self.fpY2 = -self.rectFPimage.height
+        self.fpX2 = 0
+        
+        self.moveSpeed = 5
+
+    def update(self):
+        self.fpY1 += self.moveSpeed
+        self.fpY2 += self.moveSpeed
+
+        if self.fpY1>self.rectFPimage.height:
+            self.fpY1 = -self.rectFPimage.height
+
+        if self.fpY2>self.rectFPimage.height:
+            self.fpY2 = -self.rectFPimage.height
+    
+    def render(self):
+        pantalla.blit(self.fondoPartida,(self.fpX1,self.fpY1))
+        pantalla.blit(self.fondoPartida,(self.fpX2,self.fpY2))
+
+fondo_pantalla = Fondo_Pantalla()
+
+
+
 N1 = Nave()
 M1 = Meteoritos()
+M2 = Meteoritos()
+M3 = Meteoritos()
+
+
+meteoritosGrupo = pg.sprite.Group()
+meteoritosGrupo.add(M1)
+meteoritosGrupo.add(M2)
+meteoritosGrupo.add(M3)
+
+
+fuente = pg.font.SysFont("Verdana", 60)
+gameOver = fuente.render("Game Over", True, BLANCO)
+
+score = 0
 
 while True:
+    scoreRender = fuente.render("Score: " +str(score), True, ROJO)
+    fondo_pantalla.update()
+    fondo_pantalla.render()
+    pantalla.blit(scoreRender, (0,0))
     for event in pg.event.get():
         if event.type == QUIT:
             pg.quit()
-
+        
+    if pg.sprite.spritecollide(N1, meteoritosGrupo, 0):
+        pantalla.fill(NEGRO)
+        pantalla.blit(gameOver, (250,235))
+        pg.display.update()
+        time.sleep(2)
+        pg.quit()
+    '''
+    for meteorito in meteoritosGrupo:
+        score = Meteoritos.mover(score)
+        Meteoritos.dibujar(pantalla)
+    '''
     N1.update()
-    M1.mover()
-
-    pantalla.fill(NEGRO)
+    score = M1.mover(score)
+    score = M2.mover(score)
+    score = M3.mover(score)
 
     N1.dibujar(pantalla)
     M1.dibujar(pantalla)
+    M2.dibujar(pantalla)
+    M3.dibujar(pantalla)
+
 
     pg.display.update()
     framesPerSec.tick(FPS)
